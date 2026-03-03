a8b82f89eaf5fd637a3585f2c8a41185fad5779a
NargisKhan18 <khannargis180405@gmail.com>
2026-03-03 13:35:38 +0530

    commit

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
-import ProjectCard from '../Cards/ProjectCard'
+import ProjectCard from '../Cards/ProjectCards'
 import { projects } from '../../data/constants'
 
 const Projects = ({openModal,setOpenModal}) => {
