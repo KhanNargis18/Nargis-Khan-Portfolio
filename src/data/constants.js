4b54afd7bce5f5e2bc9a445a1868b60cbe86be45
NargisKhan18 <khannargis180405@gmail.com>
2026-03-03 13:01:21 +0530

    1st commit

---
+++ src/data/constants.js
@@ -0,0 +1,310 @@
+export const Bio = {
+  name: "Nargis Khan",
+  roles: [
+    //"Full Stack Developer",
+    "Frontend Developer",
+    "Wordpress Developer",
+    //"Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/KhanNargis18",
+  resume:
+    "https://drive.google.com/file/d/1FWzYnlarARUr5IjVhJ3qSsT6RYQ4Mkth/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/khan-nargis/",
+  //insta: "",
+  //facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+      },
+      {
+        name: "Material UI (MUI)",
+        image:
+          "https://img.icons8.com/color/64/material-ui.png",
+      },
+    ],
+  },
+  {
+    title: "CMS (Content Management System)",
+    skills: [
+      {
+        name: "WordPress",
+        image:
+          "https://pngimg.com/uploads/wordpress/wordpress_PNG47.png",
+      },
+    ],
+  },
+  {
+    title: "Version Control",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://toppng.com/uploads/preview/git-logo-11609362364wwhuzvtx5h.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://w7.pngwing.com/pngs/588/441/png-transparent-github-git-hub-repository-social-icons-circular-color-icon.png",
+      },
+    ],
+  },
+  {
+    title: "Deployment / Hosting",
+    skills: [
+      {
+        name: "Netlify",
+        image:
+          "https://images.saasworthy.com/netlify_6502_logo_1586425107_zcjho.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://assets.vercel.com/image/upload/v1588805858/repositories/vercel/logo.png",
+      },
+      {
+        name: "InfinityFree",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/9032/9032137.png",
+      },
+    ],
+  },
+  {
+    title: "Development Tools",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://www.pngkit.com/png/full/296-2967652_visual-studio-code-vs-code-logo-png.png",
+      },
+    ],
+  },
+]
+
+export const experiences = [
+  {
+    id: 0,
+    role: "Fresher",
+    desc: "Worked on multiple personal and academic projects to strengthen practical knowledge of web development. Gained hands-on experience in designing responsive websites, WordPress development, frontend technologies, and SEO-friendly website while improving problem-solving and development skills.",
+  },
+  /*{
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },*/
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "/Images/Educations/Degree College.jpg",
+    school: "Niranjana Majithia College of Commerce, Kandivali West",
+    date: "June 2023 - April 2026 (Expected)",
+    grade: "8.60 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Information Technology, Niranjana Majithia College of Commerce, Kandivali west, Mumbai. I have completed 5 semesters and have a CGPA of 8.60. Completed coursework in core subjects including Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, while actively working on development projects to strengthen technical and problem-solving skills.",
+    degree: "Bachelor of Science - BSc, Information Technology",
+  },
+  {
+    id: 1,
+    img: "/Images/Educations/Junior College.jpg",
+    school: "St. Anne's High School & Junior College, Orlem Malad West",
+    date: "June 2021 - March 2023",
+    grade: "50.50%",
+    desc: "I completed my class 12 high school education at St. Anne's High School & Junior College, Orlem Malad West, Mumbai.",
+    degree: "HSC(XII), Science",
+  },
+  {
+    id: 2,
+    img: "/Images/Educations/School.jpg",
+    school: "St Mathew's High School & Junior College, Malwani Malad west",
+    date: "Apr 2021",
+    grade: "89.40%",
+    desc: "I completed my class 10 education at St Mathew's High School & Junior College, Malwani Malad west, Mumbai.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Restaurant Landing Page",
+    date: "",
+    description:
+      "A modern Restaurant Landing Page developed using React.js and Material UI that showcases menu offerings, and restaurant information in an engaging and visually appealing format. The application leverages reusable components and responsive design principles to ensure seamless performance across all devices while delivering an intuitive user experience.",
+    image:
+      "./Images/Projects/Restaurant-Landing-Page.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      "Material UI (MUI)",
+    ],
+    category: "frontend website",
+    github: "https://github.com/KhanNargis18/Restaurant-Landing-Page",
+    webapp: "https://restaurantspage.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Digital Business Website",
+    date: "",
+    description:
+      "A Digital Business Website designed to present company services, solutions, and brand identity through a structured and interactive interface. Built using HTML5, CSS3, and JavaScript, the platform provides responsive layouts, call-to-action sections, and smooth navigation to enhance user engagement. The website focuses on delivering a professional digital presence for modern businesses.",
+    image:
+      "./Images/Projects/Digital-Business-Website.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "frontend website",
+    github: "https://github.com/KhanNargis18/Digital-Business-Website",
+    webapp: "https://69a2e29b1c8f1071adf0d62e--coruscating-florentine-5dc1d5.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Blinkit UI Clone",
+    date: "",
+    description:
+      "A detailed UI clone of the Blinkit homepage built using HTML5 and CSS3, replicating real-world e-commerce design patterns. The project focuses on layout accuracy, responsive grids, navigation structure, and promotional sections to simulate a production-level grocery delivery platform interface.",
+    image:
+      "./Images/Projects/Blinkit-UI-Clone.png",
+    tags: [
+      "HTML",
+      "CSS",
+    ],
+    category: "frontend website",
+    github: "https://github.com/KhanNargis18/Blinkit-UI-Clone",
+    webapp: "https://blinkit-ui-clone.vercel.app/",
+  },
+  {
+    id: 0,
+    title: "Travel Website UI",
+    date: "",
+    description:
+      "A Travel Website UI designed to showcase destinations, travel packages, and top destination through a visually rich and structured layout. Built using HTML5 and CSS3, the interface emphasizes clean design, responsive structure, and engaging contact us components to replicate a real-world travel booking platform.",
+    image:
+      "./Images/Projects/Travel-Website-UI.png",
+    tags: ["HTML", "CSS"],
+    category: "frontend website",
+    github: "https://github.com/KhanNargis18/Travel-Website-UI",
+    webapp: "https://travel-website-ui-five.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "DevWithDev",
+    date: "",
+    description:
+      "DevWithDev is a career-focused web development platform dedicated to helping aspiring developers build real-world skills through practical learning. The platform provides structured roadmaps, hands-on projects, and mentorship guidance to support learners at every stage of their journey. Designed with a clear and user-friendly interface, DevWithDev aims to simplify complex concepts and empower individuals to grow confidently in their development careers.",
+    image:
+      "./Images/Projects/DevWithDev.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "Bootstrap",
+    ],
+    category: "frontend website",
+    github: "https://github.com/KhanNargis18/DevWithDev",
+    webapp: "https://dev-with-dev.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Random Color Generator",
+    date: "",
+    description:
+      "A simple web application that generates random background colors. When the user clicks the generate button, the app changes the background color, displays the HEX code, and automatically copies it to the clipboard.",
+    image:
+      "./Images/Projects/Random-Color-Generator.png",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "frontend website",
+    github: "https://github.com/KhanNargis18/Random-Color-Generator",
+    webapp: "https://random-color-generator-henna-nu.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Monks Event",
+    date: "",
+    description:
+      "Monks Event is a professional event management website built using WordPress and Elementor that enables businesses and clients to explore event services, view event highlights, and connect. The platform provides a visually engaging layout with service showcases, and responsive design to ensure a smooth browsing experience across devices.",
+    image:
+      "./Images/Projects/Monks-Event.png",
+    tags: ["WordPress", "Elementor", "Plugin Integration", "Navigation & Page Structuring"],
+    category: "wordpress website",
+    github: "https://monks-event.infinityfreeapp.com/wp-admin/",
+    webapp: "https://monks-event.infinityfreeapp.com/",
+  },
+  /*{
+    id: 10,
+    title: "Job Finding App",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
+    tags: ["React Native", "JavaScript", "Axios"],
+    category: "android app",
+    github: "https://github.com/rishavchanda/Job-finder-App",
+    webapp: "https://github.com/rishavchanda/Job-finder-App",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },*/
+];
+
+
